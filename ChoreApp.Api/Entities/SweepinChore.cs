42c20d1e9daab61a5b283af0bfd4cdda75cbe42b
Beafort <beafortgm@gmail.com>
2024-09-26 22:50:00 +0700

    misc

--- ChoreApp.Api/Entities/SweepinChore.cs
+++
@@ -1,6 +0,0 @@
-namespace ChoreApp.Api.Entities;
-
-public class SweepingChore : Chore
-{
-	string location {get; set;}
-}
--- ChoreApp.Api/obj/Debug/net8.0/ChoreApp.Api.AssemblyInfo.cs
+++ ChoreApp.Api/obj/Debug/net8.0/ChoreApp.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChoreApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446f028638c2c28e130901fe3161800809a88463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b057f20c2f00b72935037b4ea4ccdf6151d74cbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChoreApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChoreApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
